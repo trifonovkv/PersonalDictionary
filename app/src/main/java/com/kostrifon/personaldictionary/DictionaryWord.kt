2b85b19bdff874c6ec7ff549f6eb0d2f2d69236b
Konstantin Trifonov <trifonovkv@gmail.com>
2020-08-02 16:54:14 +0500

    ref: move printDictionaryWord function

--- app/src/main/java/com/kostrifon/personaldictionary/DictionaryWord.kt
+++ app/src/main/java/com/kostrifon/personaldictionary/DictionaryWord.kt
@@ -145,3 +145,43 @@ fun getTranslatedWord(word: String, success: (word: DictionaryWord) -> Unit, err
 fun getUniquePronunciations(dictionaryWord: DictionaryWord) = listOf(
     dictionaryWord.noun.pronunciations, dictionaryWord.verb.pronunciations, dictionaryWord.adjective.pronunciations
 ).flatten().filter { it.audioFile.isNotBlank() }.toSet().toList()
+
+fun printDictionaryWord(dictionaryWord: DictionaryWord) {
+
+    fun dictionaryEntryToString(dictionaryEntry: DictionaryEntry): String {
+        var string = ""
+
+        fun etymologiesToString(etymologies: List<String>): String {
+            if (etymologies.isEmpty()) return ""
+            var s = "\t\tEtymologies:\n"
+            etymologies.forEach { s = s.plus("\t\t\t${it}\n") }
+            return s
+        }
+
+        fun pronunciationsToString(pronunciations: List<Pronunciation>): String {
+            if (pronunciations.isEmpty()) return ""
+            var s = "\t\tPronunciations:\n"
+            pronunciations.forEach { s = s.plus("\t\t\t${it.phoneticSpelling} ${it.audioFile}\n") }
+            return s
+        }
+
+        fun translatesToString(translates: List<String>) =
+            if (translates.isEmpty()) {
+                ""
+            } else {
+                "\t\tTranslates:\n".plus("\t\t\t${translates.joinToString()}\n")
+            }
+
+        string = string.plus(translatesToString(dictionaryEntry.translates))
+        string = string.plus(pronunciationsToString(dictionaryEntry.pronunciations))
+        string = string.plus(etymologiesToString(dictionaryEntry.etymologies))
+
+        return string
+    }
+
+    println("Word: ${dictionaryWord.word}")
+    dictionaryEntryToString(dictionaryWord.noun).let { if (it.isNotBlank()) println("\tNoun:\n $it") }
+    dictionaryEntryToString(dictionaryWord.verb).let { if (it.isNotBlank()) println("\tVerb:\n $it") }
+    dictionaryEntryToString(dictionaryWord.adjective).let { if (it.isNotBlank()) println("\tAdjective:\n $it") }
+    println()
+}
